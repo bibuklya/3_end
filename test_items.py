9984384b705bec37d53b1c1348382ec66977a58d
bibuklya <bibuklya@mail.ru>
2024-03-11 21:41:20 +0500

    found button with fixture

--- test_items.py
+++ test_items.py
@@ -1,9 +1,14 @@
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_guest_should_see_button_france(browser):
-    browser.get(link)
-    time.sleep(2)
-    assert browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket") is not None, "button is not found"
+
+try:
+    def test_guest_should_see_button_france_lang_website(browser):
+        browser.get(link)
+        time.sleep(2)
+        assert browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket") is not None, "button is not found"
     
+except NoSuchElementException:
+    print("button is not found")
